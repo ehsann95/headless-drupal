bf557645bb74650dcc09dc80409405073ab932ef
ehsann95 <psychoahsan@gmail.com>
2024-01-19 15:52:22 +0530

    implement hook-form in EditArticle

--- react-app/src/components/pages/EditArticle.js
+++ react-app/src/components/pages/EditArticle.js
@@ -1,35 +1,31 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useForm, Controller } from "react-hook-form";
 import { Link, useParams } from "react-router-dom";
 import DeleteArticle from "../organisms/DeleteArticle";
 import Button from "react-bootstrap/Button";
-import Input from "../atoms/Input";
 import { userLogin } from "../../utils/auth";
 
 const auth = userLogin();
 
 function EditArticle() {
   const { id } = useParams();
-  const defaultValues = {
-    title: "",
-    body: "",
-  };
-  const [isSubmitting, setSubmitting] = useState(false);
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    register,
+    formState: { errors },
+  } = useForm();
   const [result, setResult] = useState({
     success: null,
     error: null,
     message: "",
+    title: "",
   });
-  const [values, setValues] = useState(defaultValues);
 
   useEffect(() => {
     getArticle(id);
-  }, []);
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
-  };
+  }, [id]);
 
   const getArticle = async (id) => {
     if (!id) {
@@ -37,15 +33,15 @@ function EditArticle() {
     }
 
     try {
-      const res = await axios.get(
+      const res = await auth.fetchWithAuthentication(
         `${process.env.REACT_APP_BASE_URL}jsonapi/node/article/${id}`
       );
       const data = res.data.data?.attributes;
-      let values = {
-        title: data.title,
-        body: data.body.value,
-      };
-      setValues(values);
+
+      // Use setValue to set form values
+      setValue("title", data.title);
+      setValue("body", data.body.value);
+      setResult({ ...result, title: data.title });
     } catch (err) {
       console.log("Error fetching", err);
       setResult({
@@ -56,20 +52,17 @@ function EditArticle() {
     }
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    setSubmitting(true);
-
+  const onSubmit = async (data) => {
     const fetchUrl = `jsonapi/node/article/${id}`;
 
-    let body = {
+    const body = {
       data: {
         type: "node--article",
         id: `${id}`,
         attributes: {
-          title: `${values.title}`,
+          title: `${data.title}`,
           body: {
-            value: `${values.body}`,
+            value: `${data.body}`,
             format: "plain_text",
           },
         },
@@ -86,16 +79,14 @@ function EditArticle() {
         fetchUrl,
         fetchOptions
       );
-      const data = response.data;
+      const responseData = response.data;
 
-      setSubmitting(false);
-
-      if (data.data.id) {
+      if (responseData.data.id) {
         setResult({
           success: true,
           message: (
             <div className="text-success">
-              {"Updated"}: <em>{data.data.attributes.title}</em>
+              {"Updated"}: <em>{responseData.data.attributes.title}</em>
             </div>
           ),
         });
@@ -107,17 +98,16 @@ function EditArticle() {
         error: true,
         message: <p className="text-danger">{error.message}</p>,
       });
-      setSubmitting(false);
     }
   };
 
   return (
     <>
-      <DeleteArticle id={id} title={values.title} />
+      <DeleteArticle id={id} title={result.title} />
       <div className="row top-buffer">
         <div>
           <h2>
-            Edit Article: <em>{values.title}</em>
+            Edit Article: <em>{result.title}</em>
           </h2>
         </div>
         <div className="col">
@@ -127,28 +117,52 @@ function EditArticle() {
 
           <form
             className="col-md-6 offset-md-3 text-center"
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
           >
             <div className="form-group mb-2">
-              <Input
+              <Controller
                 name="title"
-                type="text"
-                value={values.title}
-                placeholder="Title"
-                handleChange={handleInputChange}
-                required
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <input
+                    name="title"
+                    type="text"
+                    className="form-control"
+                    value={field.value}
+                    placeholder="Title"
+                    handleChange={(e) => {
+                      field.onChange(e);
+                    }}
+                    {...register("title", { required: "Required field" })}
+                  />
+                )}
               />
+              <span className="text-danger small">{errors.title?.message}</span>
             </div>
             <div className="form-group mb-2">
-              <textarea
+              <Controller
                 name="body"
-                rows="4"
-                cols="30"
-                value={values.body}
-                placeholder="Body"
-                onChange={handleInputChange}
-                className="form-control"
+                control={control}
+                defaultValue=""
+                render={({ field }) => (
+                  <textarea
+                    name="body"
+                    rows="4"
+                    cols="30"
+                    value={field.value}
+                    placeholder="Body"
+                    onChange={(e) => {
+                      field.onChange(e);
+                    }}
+                    className="form-control"
+                    {...register("body", {
+                      required: "Body field is required",
+                    })}
+                  />
+                )}
               />
+              <span className="text-danger small">{errors.body?.message}</span>
             </div>
             <div className="form-group mb-2">
               <Button type="submit" variant="primary">
